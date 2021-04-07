5fe946e7de6dbd36ca5901ce722fa1a2ee9c718c
Aashutosh <56388439+Aashutosh748@users.noreply.github.com>
2021-04-07 18:31:20 +0530

    Create 1_fibonacci_Series.py

---
+++ 1_fibonacci_Series.py
@@ -0,0 +1,33 @@
+'''Pseudo code:
+1. take input from user string.
+2. create lists to hold the strings.
+3. create empty list to hold the answer.
+4. fill the strings sets in the s1.
+5. sort the string.
+6. check if the string is less than 3.
+7. if less than 3 then dynamic.
+8. check fibbonacci series.
+9. If in series then Dynamic.
+10. if not in series then Not.
+
+'''
+
+
+for _ in range(int(input())): #input for the strings
+    s=input() #string
+    s2=list(s) #list contatining string
+    s1=list(set(s)) #list contatining string sets to match
+    a=[] #empty list to hold answer
+    for i in s1: # itirating the string in s1
+        a.append(s2.count(i)) #adding the strings to s1
+    a=sorted(a) #sorting the string
+    if(len(a) < 3): #if c1,c2,c3 less than 3 then dynamic
+        print("Dynamic") 
+    else: #if not less than 3
+        for i in range(len(a)-1, 1, -1): #checking the strings
+            if a[i] == a[i-1] + a[i-2]: #fibonacci series checker
+                print("Dynamic") #if fibonacci series then dynamic
+                break
+            else: #if not in series pattern
+                print("Not") #not dynamic
+                break
